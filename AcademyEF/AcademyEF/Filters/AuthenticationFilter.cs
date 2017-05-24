f0ab5e4dddacf47600d59691a480b7ab0f675f5f
dona1312 <donav048@gmail.com>
2017-05-24 17:47:14 +0300

    Add auth and authorization filteres, add auth service

---
+++ AcademyEF/AcademyEF/Filters/AuthenticationFilter.cs
@@ -0,0 +1,22 @@
+﻿using AcademyEF.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcademyEF.Filters
+{
+    public class AuthenticationFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (AuthenticationService.LoggedUser == null)
+            {
+                filterContext.HttpContext.Response.Redirect("~/Account/Login?redirectUrl=" + filterContext.HttpContext.Request.Url);
+                filterContext.Result = new EmptyResult();
+            }
+
+        }
+    }
+}
---
+++ AcademyEF/AcademyEF/Services/AuthenticationService.cs
@@ -0,0 +1,26 @@
+﻿using AcademyEF.Models;
+using AcademyEF.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AcademyEF.Services
+{
+    public class AuthenticationService
+    {
+        public static User LoggedUser { get; set; }
+
+        public static void Authenticate(string username, string password)
+        {
+            UsersRepository userRepo = new UsersRepository();
+            LoggedUser = userRepo.GetAll().FirstOrDefault(u => u.Username == username && u.Password == password);
+
+        }
+
+        public static void Logout()
+        {
+            LoggedUser = null;
+        }
+    }
+}
