77a439d224b56cf2dd9ec40fd281b32bc170e4be
dona1312 <donav048@gmail.com>
2017-05-24 17:03:39 +0300

    Add Repository layer

---
+++ AcademyEF/AcademyEF/Repositories/BaseRepository.cs
@@ -0,0 +1,59 @@
+﻿using AcademyEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace AcademyEF.Repositories
+{
+    public class BaseRepository<T> where T:BaseEntity, new ()
+    {
+        public DbContext Context { get; set; }
+
+        public DbSet<T> Collection { get; set; }
+        
+        public BaseRepository()
+        {
+            Context = new AcademyContext();
+            Collection = Context.Set<T>();
+        }
+
+        public List<T> GetAll(Expression<Func<T, bool>> filter = null)
+        {
+            IQueryable<T> result = Collection;
+            if (filter != null)
+                return result.Where(filter).ToList();
+            else
+                return result.ToList();
+        }
+
+        public void Add(T item)
+        {
+            Collection.Add(item);
+        }
+        public void Edit(T item)
+        {
+            Context.Entry(item).State = EntityState.Modified;
+        }
+        public void Delete(T item)
+        {
+            Collection.Remove(GetByID(item.ID));
+            Context.SaveChanges();
+        }
+        public void Save(T item)
+        {
+            if (item.ID != Guid.Empty)
+                Edit(item);
+            else
+                Add(item);
+
+            Context.SaveChanges();
+        }
+        public T GetByID(Guid id)
+        {
+            return Collection.Find(id);
+        }
+    }
+}
---
+++ AcademyEF/AcademyEF/Repositories/UsersRepository.cs
@@ -0,0 +1,12 @@
+﻿using AcademyEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AcademyEF.Repositories
+{
+    public class UsersRepository : BaseRepository<User>
+    {
+    }
+}
