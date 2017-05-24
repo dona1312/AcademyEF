9e596424a1568f50c01674c8fbc8facbf65a24f8
dona1312 <donav048@gmail.com>
2017-05-24 12:52:09 +0300

    Add models

---
+++ AcademyEF/AcademyEF/Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademyEF.Models
+{
+    public class Course : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public ICollection<User> Users { get; set; }        
+
+    }
+}
---
+++ AcademyEF/AcademyEF/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace AcademyEF.Models
+{   
+    public class User:BaseEntity
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public bool IsAdmin { get; set; }
+
+        public ICollection<Course> Courses { get; set; }
+    }
+}
