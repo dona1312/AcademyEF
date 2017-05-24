d8dda31d9af13ed252ea1ba792662c39edfe8cc9
dona1312 <donav048@gmail.com>
2017-05-24 17:24:33 +0300

    Add Service layer

---
+++ AcademyEF/AcademyEF/Services/BaseService.cs
@@ -0,0 +1,37 @@
+﻿using AcademyEF.Models;
+using AcademyEF.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace AcademyEF.Services
+{
+    public class BaseService<T> where T : BaseEntity, new()
+    {
+        private readonly BaseRepository<T> baseRepo;
+
+        public BaseService()
+        {
+            baseRepo = new BaseRepository<T>();
+        }
+
+        public List<T> GetAll(Expression<Func<T, bool>> filter = null)
+        {
+            return baseRepo.GetAll();
+        }
+        public void Save(T item)
+        {
+            baseRepo.Save(item);
+        }
+        public T GetByID(Guid id)
+        {
+            return baseRepo.GetByID(id);
+        }
+        public void Delete(Guid id)
+        {
+            baseRepo.Delete(GetByID(id));
+        }
+    }
+}
